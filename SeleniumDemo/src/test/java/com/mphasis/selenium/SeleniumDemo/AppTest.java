76be8c671f163c785722dc94819bf0e1276d71fd
Shalini Mittal <shalini06.mittal@gmail.com>
2022-03-01 13:11:31 +0530

    Day 1 Phase 5 Basic Selenium junit added

--- SeleniumDemo/src/test/java/com/mphasis/selenium/SeleniumDemo/AppTest.java
+++ SeleniumDemo/src/test/java/com/mphasis/selenium/SeleniumDemo/AppTest.java
@@ -1,20 +1,37 @@
 package com.mphasis.selenium.SeleniumDemo;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private WebDriver driver;
+    private String url;
+    private String expectedTitle;
+	
+	@BeforeEach
+	public void before()
+	{
+		//System.setProperty("webdriver.chrome.driver", "D://jars/chromedriver.exe");
+		driver = new ChromeDriver();
+		url = "http://www.google.com";
+		expectedTitle = "Google";
+		driver.navigate().to(url);
+	}	
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldTitleMatch()
+    { 	
+		String actualtitle = driver.getTitle();
+		assertEquals(actualtitle, expectedTitle);
     }
+    @AfterEach
+	public  void closeBrowser()
+	{
+		// 4) close the browser
+		driver.close();
+	}
 }
